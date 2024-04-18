f8a1c5b5479d3e3d562634a341f4c56f28ee1f84
DungBeetle0618 <{148679054}+{DungBeetle0618}@users.noreply.github.com>
2024-04-18 21:29:44 +0900

    잘합시다 시인님

---
+++ src/main/java/com/tt/talktok/TalktokApplication.java
@@ -0,0 +1,13 @@
+package com.tt.talktok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalktokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalktokApplication.class, args);
+	}
+
+}
