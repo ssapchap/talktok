391b4fbb278974c977e949cd78bc639a6746e17b
DungBeetle0618 <{148679054}+{DungBeetle0618}@users.noreply.github.com>
2024-04-24 12:40:01 +0900

    re

--- src/main/java/com/tt/talktok/entity/Lecture.java
+++ src/main/java/com/tt/talktok/entity/Lecture.java
@@ -1,13 +1,5 @@
 package com.tt.talktok.entity;
 
-<<<<<<< HEAD
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-
-import java.util.Set;
-
-=======
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -15,19 +7,14 @@ import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
->>>>>>> 9f116a1177c89e2f8675be55bff856dd51048480
 @Getter
 @Setter
 @Entity
 @Table(name = "lecture")
 public class Lecture {
     @Id
-<<<<<<< HEAD
-    private int lec_no;
-=======
     @Column(name = "lec_no")
     private int lecNo;
->>>>>>> 9f116a1177c89e2f8675be55bff856dd51048480
     private String lec_name;
     private String lec_day;
     private String lec_time;
@@ -35,10 +22,4 @@ public class Lecture {
     private String lec_detail;
     private String lec_startdate;
     private String lec_enddate;
-<<<<<<< HEAD
-//    @OneToMany(mappedBy = "lecture")
-//    private Set<Review> reviews; // 연결된 리뷰
 }
-=======
-}
->>>>>>> 9f116a1177c89e2f8675be55bff856dd51048480
--- src/main/java/com/tt/talktok/entity/Teacher.java
+++ src/main/java/com/tt/talktok/entity/Teacher.java
@@ -1,25 +1,11 @@
 package com.tt.talktok.entity;
 
 import jakarta.persistence.*;
-<<<<<<< HEAD
-import lombok.Getter;
-import lombok.Setter;
-
-import java.util.Set;
-=======
 import lombok.*;
->>>>>>> 9f116a1177c89e2f8675be55bff856dd51048480
 
 @Getter
 @Setter
 @Entity
-<<<<<<< HEAD
-@Table(name="teacher")
-public class Teacher {
-    @Id
-    @GeneratedValue
-    private int tea_no;
-=======
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -29,7 +15,6 @@ public class Teacher {
     @GeneratedValue(strategy = GenerationType.IDENTITY )
     private int tea_no;
 
->>>>>>> 9f116a1177c89e2f8675be55bff856dd51048480
     private String tea_name;
     private String tea_email;
     private String tea_pwd;
@@ -40,10 +25,5 @@ public class Teacher {
     private String tea_detail;
     private String tea_career;
     private String tea_nation;
-<<<<<<< HEAD
-//    @OneToMany(mappedBy = "teacher")
-//    private Set<Review> reviews; // 연결된 리뷰
-=======
 
->>>>>>> 9f116a1177c89e2f8675be55bff856dd51048480
 }
